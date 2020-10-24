801aa68f8e903eef2c4946aab84e5a68db97d0de
Roland Siegbert <roland@siegbert.info>
2020-10-24 14:02:00 +0200

    Configure which-key

--- config.el
+++ config.el
@@ -107,3 +107,12 @@ x-stretch-cursor t ; stretch cursor to glyph width
 (add-hook 'Info-selection-hook 'info-colors-fontify-node)
 
 (add-hook 'Info-mode-hook #'mixed-pitch-mode)
+
+(setq which-key-idle-delay 0.5) ;; I need the help, I really do
+(setq which-key-allow-multiple-replacements t)
+(after! which-key
+  (pushnew!
+   which-key-replacement-alist
+   '(("" . "\\`+?evil[-:]?\\(?:a-\\)?\\(.*\\)") . (nil . "◂\\1"))
+   '(("\\`g s" . "\\`evilem--?motion-\\(.*\\)") . (nil . "◃\\1"))
+   ))
