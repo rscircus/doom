b734b209885e89ca21c88d1b9c628465324a6f69
Roland Siegbert <roland@siegbert.info>
2020-11-04 20:00:08 +0100

    Temporariliy eliminate calctex

--- packages.el
+++ packages.el
@@ -1,8 +1,5 @@
 ;; -*- no-byte-compile: t; -*-
 
-(package! org-roam-bibtex)
-(package! org-noter)
-
 ;; ui
 (package! beacon) ; global minor mode for a blinking highliter to find where the cursor is.
 (package! rotate :pin "091b5ac4fc...") ; window mgmt
@@ -10,11 +7,19 @@
 (package! wttrin :recipe (:local-repo "lisp" :no-byte-compile t))
 (package! spray :pin "00638bc916...") ; flash words on screen
 (package! theme-magic :pin "844c4311bd...") ; terminal theme support
-(package! calctex :recipe (:host github :repo "johnbcoughlin/calctex"
-                           :files ("*.el" "calctex/*.el" "calctex-contrib/*.el" "org-calctex/*.el"))
-  :pin "7fa2673c64...")
 (package! ess-view :pin "d4e5a340b7...") ; data frames all the way down
 (package! info-colors :pin "47ee73cc19...") ; makes info pages better
+
+;; sanity
+; TODO: not ready yet: (package! origami) ; fold stuff away
+
+;; org
+(package! org-roam-bibtex)
+(package! org-noter)
+
+                                        ;(package! calctex :recipe (:host github :repo "johnbcoughlin/calctex"
+                                        ;                           :files ("*.el" "calctex/*.el" "calctex-contrib/*.el" "org-calctex/*.el"))
+                                        ; :pin "7fa2673c64...")
 (package! org-super-agenda :pin "3264255989...")
 (package! org-pretty-table-mode
   :recipe (:host github :repo "Fuco1/org-pretty-table") :pin "88380f865a...")
